c30a0c3aee8da443193a9bde725e43920a2e1226
Florian de Jong <hello@floriandejong.nl>
2026-04-05 20:52:26 +0200

    feat: add touch drag input handler

---
+++ src/systems/InputHandler.ts
@@ -0,0 +1,35 @@
+// src/systems/InputHandler.ts
+import Phaser from 'phaser';
+
+export class InputHandler {
+  offsetX: number = 0;
+  private dragging: boolean = false;
+  private startX: number = 0;
+
+  constructor(scene: Phaser.Scene) {
+    scene.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
+      this.dragging = true;
+      this.startX = pointer.x;
+      this.offsetX = 0;
+    });
+
+    scene.input.on('pointermove', (pointer: Phaser.Input.Pointer) => {
+      if (this.dragging) {
+        this.offsetX = pointer.x - this.startX;
+      }
+    });
+
+    scene.input.on('pointerup', () => {
+      this.dragging = false;
+    });
+  }
+
+  getNormalized(halfWidth: number): number {
+    return Phaser.Math.Clamp(this.offsetX / halfWidth, -1, 1);
+  }
+
+  destroy(): void {
+    this.dragging = false;
+    this.offsetX = 0;
+  }
+}
